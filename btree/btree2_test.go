b7b25408735f03c1c551dca980ed480d1cd602b0
caoting <307298900@qq.com>
2022-09-15 14:53:48 +0800

    update: btree

---
+++ btree/btree2_test.go
@@ -0,0 +1,21 @@
+package btree
+
+import (
+	"testing"
+)
+
+func TestNew(t *testing.T) {
+	//
+}
+
+func TestBPTree_Insert(t *testing.T) {
+	//
+}
+
+func TestBPTree_Find(t *testing.T) {
+	//
+}
+
+func TestBPTree_Delete(t *testing.T) {
+	//
+}
